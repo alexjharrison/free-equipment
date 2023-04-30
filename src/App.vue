2d2a7c2b40ce64d74a2a1b0d13e195d1ca7cd486
Alex Harrison <alexjharrison@gmail.com>
2023-04-30 13:38:24 -0400

    equipment site built

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="bg-sky-900 min-h-screen text-xl text-slate-300 p-4">
+    <h1 class="text-3xl font-semibold">How to homebrew 🍺</h1>
+    <p>Take your crushed grains from the homebrew store and mash them in the mash tun for ~1 hour</p>
+    <img src="/images/cooler-1.jpg" alt="mash-tun-1">
+    <p>There is a false bottom on the mash tun so wort can leave the cooler without getting clogged by the grains</p>
+    <img src="/images/cooler-2.jpg" alt="mash-tun-2">
+    <img src="/images/cooler-3.jpg" alt="mash-tun-3">
+    <p>After mashing for an hour you have converted the starches in the malts to sugars</p>
+    <p>Start the mashout by running hot liquor(hot water) from the lauter tun</p>
+    <img src="/images/lauter-tun-1.jpg" alt="lauter-tun-1">
+    <img src="/images/lauter-tun-2.jpg" alt="lauter-tun-2">
+    <p>The water flows from the later tun to the sparge arm</p>
+    <img src="/images/sparge-arm.jpg" alt="sparge-arm">
+    <p>Where it slowly trickles hot water through the grains, rinsing the sugar out of the mash tun into the boil kettle</p>
+    <img src="/images/boil-kettle-sm-1.jpg" alt="boil-kettle-sm-1">
+    <p>This is a 6 gallon kettle which works perfectly for 5 gallon batches</p>
+    <img src="/images/boil-kettle-sm-2.jpg" alt="boil-kettle-sm-2">
+    <p>I also have a 10 gallon kettle which is kinda too big and clunky but it's on offer if you want it</p>
+    <img src="/images/boil-kettle-lg-1.jpg" alt="boil-kettle-lg-1">
+    <img src="/images/boil-kettle-lg-2.jpg" alt="boil-kettle-lg-2">
+    <p>Now you boil the wort for an hour adding your hop additions at various times to dial in the right bitterness/aroma mixture</p>
+    <p>To cool down the wort to a temperature that won't boil your yeasties alive use a wort chiller hooked up to your sink or a garden hose</p>
+    <img src="/images/wort-chiller.jpg" alt="wort-chiller">
+    <p>Now that your wort is cool you need to transfer to a fermentation vessel with either</p>
+    <p>A siphon</p>
+    <img src="/images/siphon.jpg" alt="siphon">
+    <p>or a funnel (with filter to catch trub)</p>
+    <img src="/images/funnel-1.jpg" alt="funnel-1">
+    <img src="/images/funnel-2.jpg" alt="funnel-2">
+    <p>Now you have your wort in a plastic carboy</p>
+    <p>Either 6.5 gallons for 5 gal batches</p>
+    <img src="/images/carboy-lg.jpg" alt="carboy-lg">
+    <p>Or a 3 gallon carboy for smaller batches</p>
+    <img src="/images/carboy-sm.jpg" alt="carboy-sm">
+    <p>Check your starting gravity with a refractometer to see how much sugar you got from the mash and calculate the approx abv to expect</p>
+    <img src="/images/refractometer-1.jpg" alt="refractometer-1">
+    <img src="/images/refractometer-2.jpg" alt="refractometer-2">
+    <p>Pitch your yeast, throw on the bung and airlock and throw in a closet for a month</p>
+    <img src="/images/1monthlater.webp" alt="1monthlater">
+    <p>Clean and sanitize some bottles and dry on a bottle tree</p>
+    <img src="/images/bottle-tree.jpg" alt="bottle-tree">
+    <p class="mb-0">Siphon your (now) beer into your bottling bucket and add a little priming sugar to carbonate in the bottle</p>
+    <small class="text-xs">I can't locate the spigot for the bucket at the moment but I know it's around somewhere</small>
+    <img src="/images/bottling-bucket.jpg" alt="bottling-bucket">
+    <p>Don't let air into the beer at this point or it will make it taste like cardboard. Minimize turbulence while siphoning</p>
+    <p class="mb-0">Fill your bottles from the bottling bucket using a bottling wand</p>
+    <small class="text-xs">Mine's in a box somewhere but I can locate it. Looks like this</small>
+    <img src="/images/bottling-wand.jpg" alt="bottling-wand">
+    <p>Cap your bottles with a satisfying kachunk</p>
+    <img src="/images/capper-1.jpg" alt="capper-1">
+    <img src="/images/capper-2.jpg" alt="capper-2">
+    <img src="/images/oneweeklater.jpg" alt="oneweeklater">
+    <p>The bottles are carbonated and you have beer. Cheers</p>
+    <img src="/images/cheers.jpg" alt="cheers">
+    <p>Everything pictured is yours pro gratis if you want it</p>
+
+  </main>
+</template>
+
+<style>
+html,body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+img,p {
+  margin-block: 2rem;
+}
+</style>
